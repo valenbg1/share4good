b2c42eb19a659ced7bcc1758e8b9b7cd84fea008
Miguel Dorta <miguel.dorta@futuranetworks.com>
2014-04-04 23:25:15 +0200

    Vista y plantilla home

--- share4good/settings.py
+++ share4good/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webshare'
+    'webshare',
 )
 
 MIDDLEWARE_CLASSES = (
--- webshare/views.py
+++ webshare/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from webshare.models import Usuario, Organizacion, Inversion
 
 # Create your views here.
+def home(request):
+    return render_to_response('home/index.html')
