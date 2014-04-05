3393a47d2c2a4ad4b282d35cc03e428d65a6a8b0
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2014-04-05 03:03:53 +0200

    Modificaciones :)

--- webshare/urls.py
+++ webshare/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^home/$', 'webshare.views.home'),
+    url(r'^home/$', 'webshare.views.index'),
+    url(r'^home/about/$', 'webshare.views.master')
 ) 
--- webshare/views.py
+++ webshare/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render_to_response
 from webshare.models import Usuario, Organizacion, Inversion
 
 # Create your views here.
-def home(request):
-    return render_to_response('home/header.html')
+def index(request):
+    return render_to_response('layouts/index.html')
+
+def master(request):
+    return render_to_response('layouts/master.html')
